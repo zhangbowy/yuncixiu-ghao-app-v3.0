059313a6835bf9f9311bd7d9e870ce5c62c674bc
laiqiaofeng <2425138652@qq.com>
2020-09-08 11:11:06 +0800

    fix bug 预览

--- src/views/customized/commonly/components/ConfirmModal.vue
+++ src/views/customized/commonly/components/ConfirmModal.vue
@@ -1,20 +1,41 @@
 <template>
-  <van-popup :value="value" :style="{ width: '100%',height: '100vh' }" @input="val => this.$emit('input', val)">
+  <!--<van-popup :value="value" :style="{ width: '100%',height: '100vh' }" @input="val => this.$emit('input', val)">
     <div v-loading="loading" class="confirm-modal">
       <div class="confirm-content">
         <img :src="img" alt="" width="100%">
       </div>
       <div class="confirm-footer">
         <van-button size="small" color="#333" plain type="primary" @click="hidden">返回修改</van-button>
-        <!-- <van-button size="small" color="linear-gradient(to right, #ffd01e,#ff8917)">加入购物车</van-button> -->
+        <van-button size="small" color="linear-gradient(to right, #ffd01e,#ff8917)">加入购物车</van-button>
         <van-button size="small" color="linear-gradient(to right, #ff6034,#ee0a24)" @click="dobuy">立即购买</van-button>
       </div>
     </div>
-  </van-popup>
+  </van-popup> -->
+  <div v-if="value" class="preview-modal">
+    <van-image-preview
+      ref="imagePreview"
+      v-model="value"
+      v-loading="loading"
+      :images="[img]"
+      :closeable="true"
+      :async-close="true"
+      :show-index="false"
+      @click.native="onImagePreviewClick"
+    />
+    <div class="confirm-footer">
+      <van-button size="small" color="#333" plain type="primary" @click="hidden">返回修改</van-button>
+      <van-button size="small" color="linear-gradient(to right, #ff6034,#ee0a24)" @click="dobuy">立即购买</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ImagePreview } from 'vant'
+
 export default {
+  components: {
+    [ImagePreview.Component.name]: ImagePreview.Component
+  },
   props: {
     value: {
       type: Boolean,
@@ -53,21 +74,17 @@ export default {
       width: 100%;
       box-sizing: border-box;
     }
-    .confirm-footer{
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      background: #f5f5f5;
-      display: flex;
-      align-items: center;
-      padding: 0 30px;
-      height: 7vh;
-      box-sizing: border-box;
-      justify-content: space-between;
-      button{
-        width: 30%;
-      }
+  }
+  .confirm-footer{
+    position: fixed;
+    z-index: 9999;
+    left: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    bottom: 20px;
+    button{
+      width: 30%;
     }
   }
 </style>
